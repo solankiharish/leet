2f0ccf8b4264f6681c38a70afae0104605c91f39
solankiharish <160389509+solankiharish@users.noreply.github.com>
2026-09-08 15:31:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countCommas(int n) {
         if(n<999) return 0;
-        if(n>999 and n<=100000) return n-999;
+        else return n-999;
         return 0;
     }
 };
